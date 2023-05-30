ba2ffb8e03db577e893a6634c24d724d417d30c7
Prakhar Agrawal <prakharagrawal170101@outlook.com>
2023-05-30 22:13:13 +0530

    Initial commit

---
+++ app/src/main/java/prakhar17/developer/jetpackcompose/insightdaily/MainActivity.kt
@@ -0,0 +1,12 @@
+package prakhar17.developer.jetpackcompose.insightdaily
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {}
+    }
+}
